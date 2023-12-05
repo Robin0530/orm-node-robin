c2fae7338d6c9aaf85d652438f2cc4c9335db907
boram <boram910530@gmail.com>
2023-12-05 15:34:52 +0900

    feat:회원가입 페이지 추가

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/sign', function(req, res, next) {
+  res.render('sign');
+});
+
 module.exports = router;
