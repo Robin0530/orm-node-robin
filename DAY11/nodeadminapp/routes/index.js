c6b35165c1c15a7844ddb1527ad53df5b48e31d2
boram <boram910530@gmail.com>
2023-12-12 14:34:00 +0900

    채팅 개인프로젝트

---
+++ DAY11/nodeadminapp/routes/index.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+
+/* 관리자 사이트 메인-대시보드*/
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+/* 
+공통기능 제공(관리자 사이트 로그인/메인-대시보드)
+- http://localhost:3001/login
+- get
+- 로그인 페이지
+*/
+router.get('/login', async(req,res,next) =>{
+  res.render('login.ejs');
+})
+
+
+
+router.post('/login', async(req,res,next)=> {
+  let userId = req.body.adminId;
+  let userPw = req.body.password;
+
+  var member = {
+    userId,
+    userPw
+  };
+
+  console.log(`유저 input id, pw 정보 ${member}`);
+  res.redirect('/')
+})
+
+module.exports = router;
