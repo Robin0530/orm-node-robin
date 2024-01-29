ec8b63e18de8f41f8f717b90c374337fa32cca63
boram <boram910530@gmail.com>
2024-01-29 22:16:16 +0900

    day36

---
+++ DAY36/chatbasicapp/routes/index.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var router = express.Router();
+
+/* 디폴트 메인 페이지 */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+
+/*
+- 서버 소켓과 연결된 모든 사용자간 채팅하기 웹페이지 요청과 응답
+- localhost:3000/chat
+ */
+router.get('/chat', async(req, res)=> {
+
+  - res.render('chat');
+
+});
+
+
+/*
+- 특정 채팅방(채널) 사용자간 채팅하기 웹페이지 요청과 응답
+- localhost:3000/chat
+ */
+router.get('/groupchat', async(req, res)=> {
+
+  - res.render('groupchat');
+
+});
+
+module.exports = router;
